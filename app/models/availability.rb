72e0daa88b633ea870a2608879dd810a19776a1f
maangie <maangie@users.noreply.github.com>
2014-06-05 13:49:21 +0900

    sideci test

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -7,7 +7,7 @@ class Availability
   def find_by_from_time
     url = "http://www.supersaas.com/api/free/#{SCHEDULE_ID}.xml?"\
           "from=2014-06-05%2000:00:00&password=#{ADMIN_PASSWORD}"
-    
+
     doc = Document.new(open(url)).root
     Rails.logger.debug "doc: #{doc.inspect}"
   end
